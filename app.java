db9d65e751b1d728a683c392a9e658e4c1ddce79
Palika15-web <palikadua15@gmail.com>
2023-02-11 22:52:28 +0530

    app.java is a new file added

---
+++ app.java
@@ -0,0 +1,16 @@
+// Java Program to Illustrate Methods
+
+// Importing required classes
+import java.io.*;
+
+// Class 1
+// Helper class
+class Addition {
+
+// Initially taking sum as 0
+// as we have not started computation
+int sum = 0;
+
+// Method
+// To add two numbers
+public int addTwoInt(int a, int b)
